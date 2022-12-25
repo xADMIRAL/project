0c8430bd47b8837e7b87d03e248849df3da718e8
Ivan <example@gmail.com>
2022-12-25 18:28:38 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log('arr' + ' - object');
+console.log(4 + '5');
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+console.log(++incr);
+console.log(--decr);
+
+console.log(7 % 5);
